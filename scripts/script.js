1ad625783a2d2382eb8a6bcc9eef39bb970c181f
JustANipple <prandisamuel99@gmail.com>
2023-05-30 15:54:52 +0200

    modified:   index.html 	modified:   scripts/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/abstracts/_mixins.scss 	modified:   styles/scss/base/_page.scss 	modified:   styles/scss/components/_header.scss 	modified:   styles/scss/components/_pricing.scss 	modified:   styles/scss/components/_rangeBar.scss 	new file:   styles/scss/components/_switch.scss 	modified:   styles/scss/main.scss

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,21 @@
 const range = document.querySelector(`input[type="range"]`);
 const label = document.querySelector("label");
+const pageviews = document.querySelector(".pageviews_value");
+const bill = document.querySelector(".bill_value");
 range.addEventListener("input", (e) => {
+    //managing colored bar
     const target = e.target;
     const min = target.min;
     const max = target.max;
     const val = target.value;
     const newValue = (val - min) * 100 / (max - min);
-    label.style.width = "calc(" + newValue + "% - 1rem)";
+    label.style.width = newValue + "%";
+    //managing pageviews value
+    if(val < 1000) {
+        pageviews.textContent = val;
+    } else {
+        pageviews.textContent = Math.round(val / 1000) + "K";
+    }
+    //managing billing price
+    bill.textContent = "$" + Number((0.00016 * val).toFixed(2));
 });
