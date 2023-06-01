634b2a91dfad17add9ca6dac603ddc2f0c9f0ae0
JustANipple <prandisamuel99@gmail.com>
2023-06-01 17:32:31 +0200

    modified:   index.html 	modified:   scripts/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/base/_page.scss 	modified:   styles/scss/components/_header.scss 	modified:   styles/scss/components/_pricing.scss

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@ const range = document.querySelector(`input[type="range"]`);
 const label = document.querySelector("label");
 const pageviews = document.querySelector(".pageviews_value");
 const bill = document.querySelector(".bill_value");
+const switchInp = document.querySelector(".form-check-input");
+const button = document.querySelector(".btn");
+
 range.addEventListener("input", (e) => {
     //managing colored bar
     const target = e.target;
@@ -16,10 +19,26 @@ range.addEventListener("input", (e) => {
     } else {
         pageviews.textContent = Math.round(val / 1000) + "K";
     }
+
     //managing billing price
-    bill.textContent = "$" + Number((0.00016 * val).toFixed(2));
+    if(switchInp.checked) {
+        const baseBill = 0.00016 * val * 12;
+        bill.textContent = "$" + Number((baseBill - (baseBill * 25 / 100)).toFixed(0));
+    } else {
+        bill.textContent = "$" + Number((0.00016 * val).toFixed(2));
+    }
 });
 
-range.addEventListener("dragstart", () => {
-    alert();
-})
+switchInp.addEventListener("change", () => {
+    const billValue = parseInt(bill.textContent.replace("$", ""));
+    if(switchInp.checked) {
+        const billDiscount = (billValue * 12 * 25 / 100);
+        bill.textContent = "$" + Number(billValue * 12 - billDiscount).toFixed(0);
+    } else {
+        bill.textContent = "$" + Number(0.00016 * range.value).toFixed(2);
+    }
+});
+
+button.addEventListener("click", () => {
+    location.reload();
+});
